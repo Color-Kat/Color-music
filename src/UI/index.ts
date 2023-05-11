d24f176c69860dd6be07e1d1a5d13145c323cf44
Color-Kat <katik7979@gmail.com>
2023-05-11 16:57:55 +0300

    first commit

---
+++ src/UI/index.ts
@@ -0,0 +1,7 @@
+import Loader from '../UI/Loader';
+import Error from '../UI/Error';
+
+export {
+  Loader,
+  Error,
+};
---
+++ src/components/index.ts
@@ -0,0 +1,19 @@
+import Sidebar from './Sidebar';
+import Searchbar from './Searchbar';
+import SongCard from './SongCard';
+import TopPlay from './TopPlay';
+import ArtistCard from './ArtistCard';
+import DetailsHeader from './DetailsHeader';
+import SongBar from './SongBar';
+import RelatedSongs from './RelatedSongs';
+
+export {
+  TopPlay,
+  Sidebar,
+  SongCard,
+  Searchbar,
+  ArtistCard,
+  DetailsHeader,
+  SongBar,
+  RelatedSongs,
+};
