0dd38ba73e8e937dd9063f6a1cc27417c0e20432
Color-Kat <katik7979@gmail.com>
2023-05-12 21:50:16 +0300

    save volume to localStorage

--- src/modules/MusicPlayer/components/MusicPlayer.tsx
+++ src/modules/MusicPlayer/components/MusicPlayer.tsx
@@ -13,7 +13,7 @@ export const MusicPlayer = () => {
     const [duration, setDuration] = useState(0);
     const [seekTime, setSeekTime] = useState(0);
     const [appTime, setAppTime] = useState(0);
-    const [volume, setVolume] = useState(0.3);
+    const [volume, setVolume] = useState(localStorage.getItem('volume') ?? 0.3);
     const [repeat, setRepeat] = useState(false);
     const [shuffle, setShuffle] = useState(false);
     const dispatch = useDispatch();
@@ -51,6 +51,12 @@ export const MusicPlayer = () => {
             dispatch(prevSong(currentIndex - 1));
         }
     }, [shuffle, currentIndex, currentSongs]);
+
+    useEffect(() => {
+        localStorage.setItem('volume',
+            (volume > 0.01 ? volume : 0).toString()
+        );
+    }, [volume]);
 
     return (
         <div className="relative sm:px-12 px-8 w-full flex items-center justify-between">
