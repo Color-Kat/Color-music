c428c51a0cf48a8105097f6b59b7f298031ddfef
Color-Kat <katik7979@gmail.com>
2023-05-15 22:19:48 +0300

    some fixes

--- src/modules/RelatedSongs/components/SongBar.tsx
+++ src/modules/RelatedSongs/components/SongBar.tsx
@@ -2,61 +2,69 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { PlayPause } from '@components/PlayPause';
+import { MdMusicOff } from "react-icons/md";
 
 export const SongBar = ({
-                     song,
-                     i,
-                     artistId,
-                     isPlaying,
-                     activeSong,
-                     handlePauseClick,
-                     handlePlayClick
-}) => (
-    <div
-        className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}
-    >
-        <h3 className="font-bold text-base text-white mr-3">
-            {i + 1}.
-        </h3>
+                            song,
+                            i,
+                            artistId,
+                            isPlaying,
+                            activeSong,
+                            handlePauseClick,
+                            handlePlayClick
+                        }) => {
+    const coverart = (artistId
+        ? song?.attributes?.artwork?.url.replace('{w}', '125').replace('{h}', '125')
+        : song?.images?.coverart)
 
-        <div className="flex-1 flex flex-row justify-between items-center">
-            <img
-                className="w-20 h-20 rounded-lg"
-                src={artistId
-                    ? song?.attributes?.artwork?.url.replace('{w}', '125').replace('{h}', '125')
-                    : song?.images?.coverart
+    return (
+        <div
+            className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}
+        >
+            <h3 className="font-bold text-base text-white mr-3">
+                {i + 1}.
+            </h3>
+
+            <div className="flex-1 flex flex-row justify-between items-center">
+                {
+                    coverart
+                        ? <img
+                            className="w-20 h-20 rounded-lg"
+                            src={coverart}
+                            alt={song?.title}
+                        />
+                        : <MdMusicOff className="w-20 h-20 rounded-lg text-violet-400"/>
                 }
-                alt={song?.title}
-            />
 
-            <div className="flex-1 flex flex-col justify-center mx-3">
-                {!artistId ? (
-                    <Link to={`/songs/${song.key}`}>
+                <div className="flex-1 flex flex-col justify-center mx-3">
+                    {!artistId ? (
+                        <Link to={`/songs/${song.key}`}>
+                            <p className="text-xl font-bold text-white">
+                                {song?.title}
+                            </p>
+                        </Link>
+                    ) : (
                         <p className="text-xl font-bold text-white">
-                            {song?.title}
+                            {song?.attributes?.name}
                         </p>
-                    </Link>
-                ) : (
-                    <p className="text-xl font-bold text-white">
-                        {song?.attributes?.name}
+                    )}
+                    <p className="text-base text-gray-300 mt-1">
+                        {artistId ? song?.attributes?.albumName : song?.subtitle}
                     </p>
-                )}
-                <p className="text-base text-gray-300 mt-1">
-                    {artistId ? song?.attributes?.albumName : song?.subtitle}
-                </p>
+                </div>
             </div>
-        </div>
 
-        {!artistId
-            ? (
-                <PlayPause
-                    isPlaying={isPlaying}
-                    activeSong={activeSong}
-                    song={song}
-                    handlePause={handlePauseClick}
-                    handlePlay={() => handlePlayClick(song, i)}
-                />
-            )
-            : null}
-    </div>
-);
+            {!artistId
+                ? (
+                    <PlayPause
+                        isPlaying={isPlaying}
+                        activeSong={activeSong}
+                        song={song}
+                        handlePause={handlePauseClick}
+                        handlePlay={() => handlePlayClick(song, i)}
+                    />
+                )
+                : null}
+        </div>
+    );
+}
